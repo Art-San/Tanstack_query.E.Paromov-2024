0060484999b7e533f940ed6ea6e9f3f8bc108490
Art San <artsan.mcc@gmail.com>
2024-10-23 18:58:53 +0700

    status fetchStatus

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -7,18 +7,26 @@ import { useState } from 'react'
 
 export function TodoList() {
 	const [page, setPage] = useState(1)
-
+	const [enabled, setEnabled] = useState(false)
 	const {
 		data: todoItems,
 		error,
 		isPending,
+		// isFetching,
+		// isLoading,
+		status,
+		fetchStatus,
+		isPlaceholderData,
 	} = useQuery({
 		queryKey: ['tasks', 'list', { page }],
 		queryFn: (meta) => todoListApi.getTodoList({ page }, meta),
 		// placeholderData: { data: [] }, // так же можно что более сложное placeholderData: () =>  data: []
 		placeholderData: keepPreviousData, // а можно из библиотеки keepPreviousData, показываются предыдущие данные пока не появятся новые
+		enabled: enabled, // отключает включает запросник
+		// initialData: // если нужно наполнить данными к примеру из localStorage, initialData попадает в кеш, для сервер сайтрендеренга
 	})
-	// console.log(13, data)
+	console.log(10, 'status', status)
+	console.log(11, 'fetchStatus', fetchStatus)
 
 	if (isPending) {
 		return <div className="">...Loading</div>
@@ -27,8 +35,14 @@ export function TodoList() {
 		return <div className="">Error: {JSON.stringify(error)}</div>
 	}
 	return (
-		<div className="p-5 mx-auto max-w-[1200px] mt-10">
+		<div className="p-5 mx-auto max-w-[1200px] mt-10  ">
 			<h1 className="text-3xl font-bold underline mb-5">todo list</h1>
+			<button
+				onClick={() => setEnabled((enabled) => !enabled)}
+				className={enabled ? ' bg-green-300' : ' bg-gray-400'}
+			>
+				Toggle enabled
+			</button>
 			<div className=" flex flex-col gap-1">
 				<div className="flex gap-1">
 					<h4>Текущая страница:</h4>
@@ -39,11 +53,17 @@ export function TodoList() {
 					{todoItems.pages}
 				</div>
 			</div>
-			<div className=" flex flex-col gap-4">
+			<div
+				className={
+					'flex flex-col gap-4' + (isPlaceholderData ? ' opacity-30' : '')
+				} // isFetching, заменили на isPlaceholderData, но его используем тогда когда включ placeholderData: keepPreviousData
+			>
 				{todoItems.data.map((todo) => {
 					return (
 						<div className="border border-slate-300 rounded p-3" key={todo.id}>
-							{todo.text}
+							<p className={isPlaceholderData ? ' text-yellow-400' : ''}>
+								{todo.text}
+							</p>
 						</div>
 					)
 				})}
