175ec0861f430bd8ba33f449a81de2a41812e4b0
Art San <artsan.mcc@gmail.com>
2024-10-23 14:12:25 +0700

    обзор placeholderData, staleTime, gcTime

--- src/shared/api/query-client.ts
+++ src/shared/api/query-client.ts
@@ -3,7 +3,8 @@ import { QueryClient } from '@tanstack/react-query'
 export const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
-			staleTime: 1 * 60 * 1000,
+			staleTime: 1 * 60 * 1000, // время устаревания данных тут одна минута, по умолчанию 0
+			gcTime: 1000 * 60 * 5, // 5 минут по умолчанию
 		},
 	},
 })
