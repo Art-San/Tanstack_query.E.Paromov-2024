1acdb557354e1e99b1c214acbf50a5a5729ed1c9
Art San <artsan.mcc@gmail.com>
2024-10-24 21:29:52 +0700

    ok

--- src/modules/todo-list/todo-list.tsx
+++ src/modules/todo-list/todo-list.tsx
@@ -4,7 +4,7 @@ import {
 	useInfiniteQuery,
 } from '@tanstack/react-query'
 import { todoListApi } from './api'
-import { useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 
 // https://www.youtube.com/watch?v=K5-a-wjURrc&t=2523s
 
@@ -12,27 +12,28 @@ import { useState } from 'react'
 // 42:03 -- с этой может чуть дальше разбор isPending, isFetching, isLoading, status, fetchStatus
 // 1:03:18
 export function TodoList() {
-	const [page, setPage] = useState(1)
+	const cursorRef = useIntersection(() => {
+		fetchNextPage()
+	})
 	const [enabled, setEnabled] = useState(false)
 	const {
 		data: todoItems,
 		error,
-		// isPending,
-		// isFetching,
 		isLoading,
-		status,
-		fetchStatus,
 		isPlaceholderData,
-	} = useQuery({
-		queryKey: ['tasks', 'list', { page }],
-		queryFn: (meta) => todoListApi.getTodoList({ page }, meta),
-		// placeholderData: { data: [] }, // так же можно что более сложное placeholderData: () =>  data: []
-		placeholderData: keepPreviousData, // а можно из библиотеки keepPreviousData, показываются предыдущие данные пока не появятся новые
-		enabled: enabled, // отключает включает запросник
-		// initialData: // если нужно наполнить данными к примеру из localStorage, initialData попадает в кеш, для сервер сайтрендеренга
+		fetchNextPage,
+		hasNextPage,
+		isFetchingNextPage,
+	} = useInfiniteQuery({
+		queryKey: ['tasks', 'list'],
+		queryFn: (meta) => todoListApi.getTodoList({ page: meta.pageParam }, meta),
+		// enabled: enabled,
+		initialPageParam: 1,
+		getNextPageParam: (res) => res.next,
+		select: (result) => result.pages.flatMap((page) => page.data),
+		// select: (result) => result.pages.map((page) => page.data).flat(), // flat из массива массивов делает плоский массив
 	})
-	console.log(10, { status, fetchStatus })
-
+	console.log(23, todoItems)
 	if (isLoading) {
 		// isLoading нет данных но запрос идет
 		return <div className="">...Loading</div>
@@ -40,6 +41,7 @@ export function TodoList() {
 	if (error) {
 		return <div className="">Error: {JSON.stringify(error)}</div>
 	}
+
 	return (
 		<div className="p-5 mx-auto max-w-[1200px] mt-10  ">
 			<h1 className="text-3xl font-bold underline mb-5">todo list</h1>
@@ -49,22 +51,13 @@ export function TodoList() {
 			>
 				Toggle enabled
 			</button>
-			<div className=" flex flex-col gap-1">
-				<div className="flex gap-1">
-					<h4>Текущая страница:</h4>
-					{page}
-				</div>
-				<div className="flex gap-1">
-					<h4>Всего страниц:</h4>
-					{todoItems?.pages}
-				</div>
-			</div>
+
 			<div
 				className={
 					'flex flex-col gap-4' + (isPlaceholderData ? ' opacity-30' : '')
-				} // isFetching, заменили на isPlaceholderData, но его используем тогда когда включ placeholderData: keepPreviousData
+				}
 			>
-				{todoItems?.data.map((todo) => {
+				{todoItems?.map((todo) => {
 					return (
 						<div className="border border-slate-300 rounded p-3" key={todo.id}>
 							<p className={isPlaceholderData ? ' text-yellow-400' : ''}>
@@ -74,20 +67,32 @@ export function TodoList() {
 					)
 				})}
 			</div>
-			<div className=" flex gap-2 mt-4">
-				<button
-					onClick={() => setPage((p) => Math.max(p - 1, 1))}
-					className="p-3 rounded border border-teal-500"
-				>
-					prev
-				</button>
-				<button
-					onClick={() => setPage((p) => Math.min(p + 1, todoItems?.pages ?? 1))}
-					className="p-3 rounded border border-teal-500"
-				>
-					next
-				</button>
+			<div className="" ref={cursorRef}>
+				{!hasNextPage && <div>Нет данных для загрузки</div>}
+				{isFetchingNextPage && <div className="">...Loading</div>}
 			</div>
 		</div>
 	)
+}
+
+export function useIntersection(onIntersect: () => void) {
+	const unsubscribe = useRef(() => {})
+
+	return useCallback((el: HTMLDivElement | null) => {
+		const observer = new IntersectionObserver((entries) => {
+			// IntersectionObserver браузерное апи помогает отслеивать поподание элемента на экран
+
+			entries.forEach((intersection) => {
+				if (intersection.isIntersecting) {
+					onIntersect()
+				}
+			})
+		})
+		if (el) {
+			observer.observe(el)
+			unsubscribe.current = () => observer.disconnect()
+		} else {
+			unsubscribe.current()
+		}
+	}, [])
 }
