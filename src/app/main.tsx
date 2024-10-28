319be20d6cd12797b35eb22ca141ee89cf86eae7
Art San <artsan.mcc@gmail.com>
2024-10-28 22:35:51 +0700

    working offline

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,19 +2,47 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
 import './index.css'
-import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Provider } from 'react-redux'
 import { queryClient } from '../shared/api/query-client'
 import { store } from '../shared/redux'
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+
+const persister = createSyncStoragePersister({
+	storage: window.localStorage,
+})
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
+		<PersistQueryClientProvider
+			client={queryClient}
+			persistOptions={{ persister }}
+			onSuccess={() => {
+				// возобновить мутации после успешного первоначального восстановления из localStorage
+				queryClient.resumePausedMutations().then(() => {
+					queryClient.invalidateQueries()
+				})
+			}}
+		>
 			<Provider store={store}>
 				<App />
 			</Provider>
 			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		</PersistQueryClientProvider>
 	</StrictMode>
 )
+
+//  <PersistQueryClientProvider
+// client={queryClient}
+// persistOptions={{ persister }}
+// onSuccess={() => {
+// 	// resume mutations after initial restore from localStorage was successful
+// 	queryClient.resumePausedMutations().then(() => {
+// 		queryClient.invalidateQueries()
+// 	})
+// }}
+// >
+// <Movies />
+// <ReactQueryDevtools initialIsOpen />
+// </PersistQueryClientProvider>
--- src/shared/api/query-client.ts
+++ src/shared/api/query-client.ts
@@ -4,7 +4,7 @@ export const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
 			staleTime: 1 * 60 * 1000, // время устаревания данных тут одна минута, по умолчанию 0
-			gcTime: 1000 * 60 * 5, // 5 минут по умолчанию
+			gcTime: 24 * 60 * 60 * 1000, // 24 часа
 		},
 	},
 })
